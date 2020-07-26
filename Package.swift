95ed5662154868d6614d8d97d09e5b55a2656b35
Alex Babaev <alex@devpocket.com>
2020-07-26 15:57:49 +0300

    Removed Example from Swift Package to avoid confusion
    Added separate frameworks for iOS/macOS (for Carthage)

--- Package.swift
+++ Package.swift
@@ -6,12 +6,10 @@ let package = Package(
     name: "Macaroni",
     platforms: [ .iOS(.v11), .macOS(.v10_14) ],
     products: [
-        .library(name: "Macaroni", type: .static, targets: [ "Macaroni" ]),
-        .executable(name: "Example", targets: [ "Example" ]),
+        .library(name: "Macaroni", type: .static, targets: [ "Macaroni" ])
     ],
     targets: [
-        .target(name: "Macaroni", dependencies: [], path: "Sources"),
-        .target(name: "Example", dependencies: [ "Macaroni" ], path: "Example")
+        .target(name: "Macaroni", dependencies: [], path: "Sources")
     ],
     swiftLanguageVersions: [.v5]
 )

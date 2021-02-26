ceb7c9f2cfe288a80d63b564024c368b8319cf4e
Alex Babaev <alex@jdnevnik.com>
2021-02-26 17:21:09 +0300

    Tests are better structured.

---
+++ Tests/MacaroniTests/SimpleContainerWithParameterTests.swift
@@ -0,0 +1,42 @@
+import XCTest
+@testable import Macaroni
+
+class SimpleContainerWithParameterTests: XCTestCase {
+    private class TestInjectedType {
+        var control: Int
+
+        init(control: Int) {
+            self.control = control
+        }
+    }
+
+    private var container: SimpleContainer!
+
+    override func setUp() {
+        super.setUp()
+
+        container = SimpleContainer()
+        container.register { (control: Any) -> TestInjectedType in
+            guard let control = control as? Int else { fatalError("Ouch") }
+            return TestInjectedType(control: control)
+        }
+    }
+
+    func testRegistrationWithParameterGetWithoutParameter() {
+        let value: TestInjectedType?
+        do {
+            value = try container.resolve()
+            XCTAssertNil(value, "Value is wrongly resolved without parameter")
+        } catch {
+            XCTAssertNotNil(error, "Value is wrongly resolved without parameter")
+        }
+    }
+
+    func testRegistrationWithParameter() throws {
+        let controlValue = Int.random(in: Int.min ... Int.max)
+        let value: TestInjectedType? = try container.resolve(parameter: controlValue)
+
+        XCTAssertNotNil(value, "Could not resolve value")
+        XCTAssertEqual(value?.control, controlValue, "Resolved value is wrong")
+    }
+}
--- Tests/MacaroniTests/SimpleTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import Macaroni
-
-class SimpleTests: XCTestCase {
-    func testSimple() throws {
-        XCTAssertTrue(true, "wat?")
-    }
-}
